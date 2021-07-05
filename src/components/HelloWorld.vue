44b96421c3239bd77abd153f7a7ea3931b1e0f6f
hirabayashi <hirabayashi@amhard.co.jp>
2021-07-05 21:39:40 +0900

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,82 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="counter++" id="counter">Counter: {{ counter }}</button>
+  <div class="prof">
+    <div class="face">
+      <img src="../assets/P_B2C1_0316_182849.jpg" alt="顔写真" />
+    </div>
+    <div class="name">
+      <p>
+        平林　彰史
+      </p>
+      <p>1983年6月24日、東京都の小平市に生まれる。<br /></p>
+    </div>
+  </div>
+
+  <p>
+    Recommended IDE setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    +
+    <a
+      href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
+      target="_blank"
+    >
+      Vetur
+    </a>
+    or
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    (if using
+    <code>&lt;script setup&gt;</code>)
+  </p>
+
+  <p>See <code>README.md</code> for more information.</p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Docs
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+  </p>
+
+  <button @click="counter--">count is: {{ counter }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      counter: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
